8b6bcfdbcc827fae1ce6ea05f3aa28e96af0bb61
basit3407 <basit3407@gmail.com>
2024-06-16 10:20:46 +0500

    changing structure in next commit

--- cortex_yen/cortex_yen/settings.py
+++ cortex_yen/cortex_yen/settings.py
@@ -158,8 +158,9 @@ django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.yourmailserver.com"
-EMAIL_PORT = 587  # or 465 for SSL
-EMAIL_HOST_USER = "your_email@example.com"
-EMAIL_HOST_PASSWORD = "your_email_password"
-EMAIL_USE_TLS = True  # or EMAIL_USE_SSL = True if using SSL
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = "your_email@example.com"
