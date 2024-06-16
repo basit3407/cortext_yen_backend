ea84e79c34a39cb21a7d4be39d7e546415eb490a
basit3407 <basit3407@gmail.com>
2024-06-16 11:37:47 +0500

    redeploy

--- cortex_yen/settings/production.py
+++ cortex_yen/settings/production.py
@@ -2,6 +2,6 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://corleebackend-05d62e3e59f9.herokuapp.com/']
+ALLOWED_HOSTS = ['corleebackend-05d62e3e59f9.herokuapp.com']
 
 # Ensure you add any other production-specific settings here
