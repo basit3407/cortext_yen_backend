bd6c8dac241d9f3e03ae9bbcd0c7522ef4420104
basit3407 <basit3407@gmail.com>
2024-06-16 11:34:56 +0500

    testing

--- cortex_yen/settings/production.py
+++ cortex_yen/settings/production.py
@@ -2,8 +2,6 @@ from .base import *
 
 DEBUG = False
 
-# ALLOWED_HOSTS = ['your-production-domain.com']
-
-ALLOWED_HOSTS = [] 
+ALLOWED_HOSTS = ['https://corleebackend-05d62e3e59f9.herokuapp.com/']
 
 # Ensure you add any other production-specific settings here
