36532566c6f6f31a2729f5d78f87d3662916639f
basit3407 <basit3407@gmail.com>
2024-06-20 16:52:27 +0500

    success. search functionality added

--- cortex_yen_app/views.py
+++ cortex_yen_app/views.py
@@ -332,6 +332,16 @@ class FabricListAPIView(generics.ListAPIView):
         ],
         responses={200: FabricSerializer(many=True)},
     )
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     def get_queryset(self):
         queryset = Fabric.objects.all()
         keyword = self.request.GET.get("keyword", "").lower()
