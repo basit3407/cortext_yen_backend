6015e5d7e962ac800790cb9c3590a75a6baae7d9
basit3407 <basit3407@gmail.com>
2024-07-23 09:58:13 +0500

    added order in admin panel for testing

--- cortex_yen_app/admin.py
+++ cortex_yen_app/admin.py
@@ -7,31 +7,29 @@ from .models import (
     Fabric,
     Cart,
     Favorite,
-    # Order,
-    # OrderItem,
+    Order,
+    OrderItem,
     MediaUploads,
-    # Order,
-    # OrderItem,
 )
 
 
-# class OrderItemInline(admin.TabularInline):
-#     model = OrderItem
-#     extra = 1
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
 
 
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ("customer_name", "customer_email", "request_number")
-#     inlines = [OrderItemInline]
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("customer_name", "customer_email", "request_number")
+    inlines = [OrderItemInline]
 
-#     def save_model(self, request, obj, form, change):
-#         # Save the order first to generate the ID
-#         super().save_model(request, obj, form, change)
-#         # Ensure the request_number is generated if it does not exist
-#         if not obj.request_number:
-#             obj.request_number = obj.generate_request_number()
-#             obj.save()
+    def save_model(self, request, obj, form, change):
+        # Save the order first to generate the ID
+        super().save_model(request, obj, form, change)
+        # Ensure the request_number is generated if it does not exist
+        if not obj.request_number:
+            obj.request_number = obj.generate_request_number()
+            obj.save()
 
 
 admin.site.register(CustomUser)
